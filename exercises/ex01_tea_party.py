1b379dd4b997f5865396a9be2e4d84f543c6ed38
MacBeing3 <162499903+MacBeing3@users.noreply.github.com>
2025-01-27 09:13:25 -0500

    some change with explicit argument in function call

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -12,7 +12,12 @@ def main_planner(guests: int) -> None:
 
     print("Treats: " + str(treats(people=guests)))
 
-    print("Cost: $" + str(cost(tea_count=tea_bags(guests), treat_count=treats(guests))))
+    print(
+        "Cost: $"
+        + str(
+            cost(tea_count=tea_bags(people=guests), treat_count=treats(people=guests))
+        )
+    )
 
 
 def tea_bags(people: int) -> int:
@@ -24,7 +29,7 @@ def tea_bags(people: int) -> int:
 def treats(people: int) -> int:
     """Calcs the num of treats per tea had"""
 
-    return int(tea_bags(people) * 1.5)
+    return int(tea_bags(people=people) * 1.5)
 
 
 def cost(tea_count: int, treat_count: int) -> float:
