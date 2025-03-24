9cc34a924b60e620ff3d1a9103ea833636fce321
MacBeing3 <162499903+MacBeing3@users.noreply.github.com>
2025-03-24 09:55:05 -0400

    in class work

--- cl24_dictionaries.py
+++ cl24_dictionaries.py
@@ -10,3 +10,17 @@ ice_cream: dict[str, int] = {"chocolate": 12, "vanilla": 8, "strawberry": 4}
 ice_cream["vanilla"] += 110
 
 ice_cream["mint"] = 3
+
+print(ice_cream["chocolate"])
+ice_cream["vanilla"] = 10
+
+
+if "mint" in ice_cream:
+    print("orders of mint = ", ice_cream["mint"])
+
+else:
+    print("no orders of mint")
+
+
+ice_cream.pop("strawberry")
+print(ice_cream)
